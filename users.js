b2b128d5a6d5d7f31c8f29eaa54f684f06afe88f
EhsanRahimi1396 <72453993+EhsanRahimi1396@users.noreply.github.com>
2020-10-14 12:21:14 +0330

    Add files via upload

---
+++ users.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt =require('jsonwebtoken');
+
+const User = require('../models/user');
+
+
+exports.User_signup = (req, res, next) => {
+    User.find({ email: req.body.email }) /// ایمیل را میگیره و چک میکنه اگه در دیتا بیس وجود داشت اخطار میده و اگه وجود نداشت یک اکانت ایجاد میکنه
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "mail exists"
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: 'user created'
+                                })
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                })
+            }
+        })
+    };
+    exports.User_login =(req,res,next )=>{
+        User.find({email :req.body.email})
+        .exec()
+        .then( user =>{
+            if(user.length <1){
+                return res.status(404).json({
+                    message:"mail not found, user doesnt exist"
+                });
+            }
+            bcrypt.compare(req.body.password,user[0].password,(err,res)=>{
+                if(err ){
+                    return res.status(401).json({
+                        message:"Auto faild",
+                    });
+    
+                }if (result){
+                   const token= jwt.sing({ 
+                    email:user[0].email,
+                    userId:user[0]._id    
+                },process.env.JWT_KEY ,
+                {
+                    expiresIn:"1h"
+                })
+                    return res.status(200).json({
+                        message:"Auth successful",
+                        token:token
+    
+                    })
+                }
+                return res.status(401).json({
+                    message:"Auto faild",
+            });
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        })
+    })
+};
+exports.User_delete =(req,res,next) =>{
+    User.remove({_id:req.params.userId}).exec().then(result =>{ 
+        res.status(200).json({
+            message:"User Deleted"
+        });
+
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+    });
+}
